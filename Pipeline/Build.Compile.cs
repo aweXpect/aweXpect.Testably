50c77b73275f55e3ad0d86f860587bfc111e3c77
Valentin <valentin.breuss@baur.eu>
2025-01-31 10:42:48 +0100

    feat: support pre-release packages
    Allow creating pre-release nuget packages by appending e.g. "-pre.1" to the tag

--- Pipeline/Build.Compile.cs
+++ Pipeline/Build.Compile.cs
@@ -1,11 +1,11 @@
+using System;
+using System.Linq;
 using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities;
 using Nuke.Common.Utilities.Collections;
 using Serilog;
-using System;
-using System.Linq;
-using Nuke.Common.Utilities;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 // ReSharper disable AllUnderscoreLocalParameterName
@@ -62,16 +62,27 @@ partial class Build
 		.DependsOn(CalculateNugetVersion)
 		.Executes(() =>
 		{
+			string preRelease = "-CI";
+			if (GitHubActions == null)
+			{
+				preRelease = "-DEV";
+			}
+			else if (GitHubActions.Ref.StartsWith("refs/tags/", StringComparison.OrdinalIgnoreCase))
+			{
+				int preReleaseIndex = GitHubActions.Ref.IndexOf('-');
+				preRelease = preReleaseIndex > 0 ? GitHubActions.Ref[preReleaseIndex..] : "";
+			}
+
 			ReportSummary(s => s
 				.WhenNotNull(SemVer, (summary, semVer) => summary
-					.AddPair("Version", semVer)));
+					.AddPair("Version", semVer+ preRelease)));
 
 			DotNetBuild(s => s
 				.SetProjectFile(Solution)
 				.SetConfiguration(Configuration)
 				.EnableNoLogo()
 				.EnableNoRestore()
-				.SetVersion(SemVer)
+				.SetVersion(SemVer+ preRelease)
 				.SetAssemblyVersion(GitVersion.AssemblySemVer)
 				.SetFileVersion(GitVersion.AssemblySemFileVer)
 				.SetInformationalVersion(GitVersion.InformationalVersion));
